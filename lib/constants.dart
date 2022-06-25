75e6c3bcc65f6c8e4ccb7010cfc37ed90cefa533
Louise <louiserafaellalu@gmail.com>
2022-06-25 20:17:54 +0800

    +1: make constants.dart

---
+++ lib/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+/// Colors
+const kOffWhiteColor = Color(0xFFF5F5F5);
+const kLightGrayColor = Color(0xFFEBEBEB);
+const kGrayColor = Color(0XFFD9D9D9);
+const kDarkGrayColor = Color(0xFF737373);
+const kPurpleColor = Color(0xFF5E17EB);
+const kDarkPurpleColor = Color(0xFF571BCE);
+const kGreenishColor = Color(0xFFA4B8C4);
