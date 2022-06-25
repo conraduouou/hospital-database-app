acfa1aee0a9f86526e800e47ddf50979c0dc03b2
Louise <louiserafaellalu@gmail.com>
2022-06-25 19:59:13 +0800

    +1: depend on window_size package and remove ios and android folders

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:hospital_database_app/hospital_app.dart';
+import 'package:window_size/window_size.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (Platform.isMacOS) {
+    setWindowTitle('Hospital Database App');
+    setWindowMinSize(const Size(1280, 720));
+  }
   runApp(const HospitalApp());
 }
